bcce264c8aaf3fb447cc19ba33ddb6c753362ecd
Andreas Winther Moen <andreas@winthermoen.no>
2022-09-07 23:26:10 +0200

    Optimize event filtering by checking whether is attendance event

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from online import get_event_list, event_is_in_the_future
 from db import Database
 
+def is_relevant_event(event, db):
+    if event['is_attendance_event'] == False:
+        return False
+    if not event_is_in_the_future(event['id']):
+        return False
+    if db.event_exists_in_database(event['id']):
+        return False
+    return True
+
 def discover_new_bedpres_and_add_to_database(db):
     events = get_event_list()
     print(f"received {len(events)} events")
-    events = [e for e in events if event_is_in_the_future(e['id']) and not db.event_exists_in_database(e['id'])]
+    events = [event for event in events if is_relevant_event(event, db)]
     print(f"filtered all events to {len(events)} events")
     db.add_events_to_database(events)
 
