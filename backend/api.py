5041f1c5e15387860b6c5383dcc7856dc16d5d08
Andreas Winther Moen <andreas@winthermoen.no>
2022-09-16 12:36:19 +0200

    Add logging to api module when running directly

--- backend/api.py
+++ backend/api.py
@@ -50,4 +50,6 @@ def remove_subscriber(number):
     database.disconnect()
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s')
+    logging.info('Starting API...')
     api.run()
