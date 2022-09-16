902d57d3b19c9e5032e60e8b854b271e6217bf9b
Andreas Winther Moen <andreas@winthermoen.no>
2022-09-16 12:25:08 +0200

    Add try catch block in OW requests

--- backend/online.py
+++ backend/online.py
@@ -1,10 +1,15 @@
 from requests import get
 from utils import date_is_in_the_future
+import logging
 
 def get_event_list():
-    response = get('https://old.online.ntnu.no/api/v1/event/events/?format=json&event_end__gte=2022-09-07&page_size=50', timeout=30)
-    data = response.json()
-    return data['results']
+    try:
+        response = get('https://old.online.ntnu.no/api/v1/event/events/?format=json&event_end__gte=2022-09-07&page_size=50', timeout=30)
+        data = response.json()
+        return data['results']
+    except:
+        logging.error('Failed to fetch events from OW')
+        return []
 
 def event_is_in_the_future(event_id):
     try:
@@ -13,11 +18,16 @@ def event_is_in_the_future(event_id):
         registration_start = data['registration_start']
         return registration_start is not None and date_is_in_the_future(registration_start)
     except:
+        logging.error(f'Failed to fetch event {event_id} from OW')
         return False
 
 def add_registration_start_to_event(event):
-    response = get(f'https://old.online.ntnu.no/api/v1/event/attendance-events/{event["id"]}/?format=json', timeout=30)
-    data = response.json()
-    registration_start = data['registration_start']
-    event['registration_start'] = registration_start
-    return event
+    try:
+        response = get(f'https://old.online.ntnu.no/api/v1/event/attendance-events/{event["id"]}/?format=json', timeout=30)
+        data = response.json()
+        registration_start = data['registration_start']
+        event['registration_start'] = registration_start
+        return event
+    except:
+        logging.error(f'Failed to fetch event {event["id"]} from OW')
+        return event
