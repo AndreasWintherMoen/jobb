7441f5e139e2818047d4038b87b963e1d33567f9
Andreas Winther Moen <andreas@winthermoen.no>
2022-09-29 12:51:38 +0200

    Parse scheduler delay as int and add format message function to utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -12,8 +12,19 @@ def get_delay_until_five_minutes_before_event(date):
     now = datetime.now(timezone)
     event_time = now.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
     run_at = event_time - timedelta(minutes=5)
-    delay = (run_at - now).total_seconds()
+    delay = int((run_at - now).total_seconds())
     return delay
+
+def format_message_for_events(events):
+    titles = [event['title'] for event in events]
+    formatted_titles = ''
+    if (len(titles) == 1):
+        formatted_titles = f"'{titles[0]}'"
+    elif (len(titles) == 2):
+        formatted_titles = f"'{titles[0]}' og '{titles[1]}'"
+    else:
+        formatted_titles = "'" + f"', '".join(titles[:-1]) + f" og '{titles[-1]}'"
+    return f"Påmelding til {formatted_titles} åpner om 5 minutter"
 
 def get_current_date():
     return datetime.now(timezone)
