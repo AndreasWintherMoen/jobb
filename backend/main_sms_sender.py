3212fac8b2026b996220108944cf5d8137bb5282
Andreas Winther Moen <andreas@winthermoen.no>
2022-09-29 10:51:25 +0200

    Add main file for sms sender function

---
+++ backend/main_sms_sender.py
@@ -0,0 +1,22 @@
+from sms import send_multiple_sms
+from db import Database
+import logging
+import google.cloud.logging
+
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s')
+gc_logging_client = google.cloud.logging.Client()
+gc_logging_client.setup_logging()
+
+def sms_endpoint(data):
+    logging.info("********* SMS ENDPOINT CALLED... *********")
+    try:
+        message = data.json["message"]
+        database = Database()
+        database.connect()
+        phone_numbers = database.get_phone_numbers_from_database()
+        database.disconnect()
+        send_multiple_sms(message, phone_numbers)
+        return f"Message successfully sent to {len(phone_numbers)} phone numbers"
+    except Exception as e:
+        logging.error(e)
+        return "Error sending SMS. See logs for info"
