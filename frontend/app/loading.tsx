db98dbb73b7759e6b9b5d55a51c61e1c2a30dc89
Andreas Winther Moen <andreas@winthermoen.no>
2023-01-25 14:04:49 +0100

    Add loading component to root app folder

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,4 +1,4 @@
-import LoadingSpinner from '../../components/LoadingSpinner';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 export default function Loading() {
   return <LoadingSpinner />;
