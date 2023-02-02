affc5117a13643a91a8e9df024fa8f01fa670e01
Andreas Winther Moen <andreas@winthermoen.no>
2023-02-02 16:12:03 +0100

    Remove commented out code in register page

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,22 +1,11 @@
 export const dynamic = 'force-dynamic';
 
-// import React, { useState } from 'react';
 import auth from '../../auth';
 
 import { headers } from 'next/headers';
 import RegisterSteps from './registerSteps';
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export default async function RegisterPage(stuff: any) {
-  const nextHeaders = headers();
-  const token = nextHeaders.get('x-ow-token');
-
-  // const [step, setStep] = useState(0);
-  let step = 1;
-  console.log('page.tsx', stuff);
-
-  // await delay(5000);
+export default async function RegisterPage() {
   let owUser: any = {};
   let token: string | null = '';
   try {
@@ -24,7 +13,6 @@ export default async function RegisterPage(stuff: any) {
     token = nextHeaders.get('x-ow-token');
     owUser = await auth.fetchFullProfile(token || undefined);
   } catch (e: any) {
-    console.error(e);
     return (
       <div>
         <p>No user</p>
@@ -38,13 +26,4 @@ export default async function RegisterPage(stuff: any) {
   }
 
   return <RegisterSteps owUser={owUser} />;
-
-  // const user = await database.fetchUser(1421);
-  // if (!user || !user.phone_number) {
-  //   return <div>no user</div>;
-  // }
-
-  // if (step === 0) {
-  //   return <PhoneInput />; // TODO: Implement this
-  // }
 }
