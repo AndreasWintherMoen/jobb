516e5080d6597ea270334ed098d8417f6f06e88d
Andreas Winther Moen <andreas@winthermoen.no>
2023-02-07 11:37:28 +0100

    Validate http responses to be 2xx

--- backend/online.py
+++ backend/online.py
@@ -13,6 +13,9 @@ def get_event_list() -> List[Event]:
     url = f'https://old.online.ntnu.no/api/v1/event/events/?format=json&event_end__gte={date}&page_size=50'
     try:
         response = get(url)
+        if not response.ok:
+            logger.error(f'Failed to fetch events from OW. Received status code {response.status_code}')
+            return []
         data = response.json()
         return data['results']
     except:
@@ -24,6 +27,9 @@ def event_is_in_the_future(event: Event) -> bool:
         event_id = event['id']
         start_date = event['start_date']
         response = get(f'https://old.online.ntnu.no/api/v1/event/attendance-events/{event_id}/?format=json', timeout=30)
+        if not response.ok:
+            logger.error(f'Failed to fetch attendance event {event["id"]} from OW. Received status code {response.status_code}')
+            return False
         data = response.json()
         registration_start = data['registration_start']
         if registration_start is None or not date_is_in_the_future(registration_start):
@@ -38,6 +44,9 @@ def event_is_in_the_future(event: Event) -> bool:
 def add_registration_dates_to_event(event: Event) -> Event:
     try:
         response = get(f'https://old.online.ntnu.no/api/v1/event/attendance-events/{event["id"]}/?format=json', timeout=30)
+        if not response.ok:
+            logger.error(f'Failed to fetch attendance event {event["id"]} from OW. Received status code {response.status_code}')
+            return event
         data = response.json()
         registration_start = data['registration_start']
         registration_end = data['registration_end']
@@ -54,6 +63,9 @@ def get_attendees_for_event(event: Event) -> List[EventAttendee]:
     url = f'https://old.online.ntnu.no/api/v1/event/attendance-events/{event["id"]}/public-attendees/?format=json'
     try:
         response = get(url, timeout=30, headers=__get_auth_headers())
+        if not response.ok:
+            logger.error(f'Failed to fetch attendance event {event["id"]} from OW. Received status code {response.status_code}')
+            return []
         users = response.json()
         return users
     except:
